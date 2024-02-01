60c9169fef970cb2533be12d45a0b25e5d818a71
maxgra1 <maksgrod54@gmail.com>
2024-01-31 23:14:50 -0500

    New Task

--- cypress/e2e/Negative tests/New Lecture Negative.cy.js
+++ cypress/e2e/Negative tests/New Lecture Negative.cy.js
@@ -2,14 +2,109 @@
 beforeEach(() => {
   cy.viewport(1920, 1080)
 });
+describe('Guest Tests', () => {
+it('Guest option is chosen without selecting when', () => {
+  cy.Log12();
+  cy.wait(2000);
+  cy.get(':nth-child(4) > .popup-with-zoom-anim').click();
+  cy.get(':nth-child(2) > .category-box').click();
+  cy.get('#start-session-step-2 > .copy-submit-box').click();
+  cy.get('#start-session-step-2 > .notification').should('be.visible')
+});
+it('Guest option is chosen without selecting time and date', () => {
+  cy.Log12();
+  cy.wait(2000);
+  cy.get(':nth-child(4) > .popup-with-zoom-anim').click();
+  cy.get(':nth-child(2) > .category-box').click();
+  cy.get(':nth-child(2) > label').click();
+  cy.get('#start-session-step-2 > .copy-submit-box').click();
+  cy.get('#start-session-step-2 > .notification').should('be.visible')
+  cy.pause();
+});
+it('Guest option is chosen without selecting time', () => {
+  cy.Log12();
+  cy.wait(2000);
+  cy.get(':nth-child(4) > .popup-with-zoom-anim').click();
+  cy.get(':nth-child(2) > .category-box').click();
+  cy.get(':nth-child(2) > label').click();
+  cy.get('.datepicker').type('13.03.2029')
+  cy.get('#start-session-step-2 > .copy-submit-box').click();
+  cy.get('#start-session-step-2 > .notification').should('be.visible')
+});
+it('Guest option is chosen without selecting date', () => {
+  cy.Log12();
+  cy.wait(2000);
+  cy.get(':nth-child(4) > .popup-with-zoom-anim').click();
+  cy.get(':nth-child(2) > .category-box').click();
+  cy.get(':nth-child(2) > label').click();
+  cy.get('.clockpicker').type('00:05')
+  cy.get('#start-session-step-2 > .copy-submit-box').click();
+  cy.get('#start-session-step-2 > .notification').should('be.visible')
+});
 
-describe('New Lecture Visible', () => {
-    iit('New Lecture dialog window is not opened if incorrect element clicked', () => {
-      cy.Log12();
-      cy.wait(2000);
-      // Клік на невірний елемент, який не має відкривати вікно лекції
-      cy.get('#incorrect-element').click();
-      // Перевірка, що вікно лекції не відкрите
-      cy.get('#start-session-step-1 > .welcome-text > h3').should('not.exist');
+});
+describe('My Connections Tests', () => {
+  it('My Connections no option is chosen ', () => {
+    cy.Log12();
+    cy.wait(2000);
+    cy.get(':nth-child(4) > .popup-with-zoom-anim').click();
+    cy.get(':nth-child(1) > .category-box').click();
+    cy.get('#start-session-step-2 > .copy-submit-box').click();
+    cy.get('#start-session-step-2 > .notification').should('be.visible')
   });
-})
+  it('My Connections choose only connections', () => {
+    cy.Log12();
+    cy.wait(2000);
+    cy.get(':nth-child(4) > .popup-with-zoom-anim').click();
+    cy.get(':nth-child(1) > .category-box').click();
+    cy.get('.participant-check').click();
+    cy.get('#start-session-step-2 > .copy-submit-box').click();
+    cy.get('#start-session-step-2 > .notification').should('be.visible')
+    
+  });
+  it('My Connections choose only when start "Now"', () => {
+    cy.Log12();
+    cy.wait(2000);
+    cy.get(':nth-child(4) > .popup-with-zoom-anim').click();
+    cy.get(':nth-child(1) > .category-box').click();
+    cy.get('.user-type-radio > :nth-child(1) > label')
+    cy.get(':nth-child(2) > label').click();
+    cy.get('#start-session-step-2 > .copy-submit-box').click();
+    cy.get('#start-session-step-2 > .notification').should('be.visible')
+    
+  });
+  it('My Connections choose only when start "Later"', () => {
+    cy.Log12();
+    cy.wait(2000);
+    cy.get(':nth-child(4) > .popup-with-zoom-anim').click();
+    cy.get(':nth-child(1) > .category-box').click();
+    cy.get(':nth-child(2) > label').click();
+    cy.get('#start-session-step-2 > .copy-submit-box').click();
+    cy.get('#start-session-step-2 > .notification').should('be.visible')
+    
+  });
+  it('My Connections try to start without select time', () => {
+    cy.Log12();
+    cy.wait(2000);
+    cy.get(':nth-child(4) > .popup-with-zoom-anim').click();
+    cy.get(':nth-child(1) > .category-box').click();
+    cy.get('.participant-check').click();
+    cy.get(':nth-child(2) > label').click();
+    cy.get('#start-session-step-2 > .copy-submit-box').click();
+    cy.get('#start-session-step-2 > .notification').should('be.visible')
+    
+  });
+  it('My Connections try to start without Connections Groups', () => {
+    cy.Log12();
+    cy.wait(2000);
+    cy.get(':nth-child(4) > .popup-with-zoom-anim').click();
+    cy.get(':nth-child(1) > .category-box').click();
+    cy.get('.user-type-radio > :nth-child(1) > label').click();
+    cy.get('[data-participant-type-target-selection="connection-groups"]').click();
+    cy.get('#start-session-step-2 > .copy-submit-box').click();
+    cy.get('#start-session-step-2 > .notification').should('be.visible')
+    
+  });
+  
+  });
+
